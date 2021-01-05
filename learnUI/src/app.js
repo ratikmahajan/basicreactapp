f28c796de785ee443874e14c5530b5f757900ed9
Ratik Mahajan <ranuj8789@gmail.com>
2021-01-05 21:43:18 +0530

    First Commit
    first commint

---
+++ learnUI/src/app.js
@@ -0,0 +1,4 @@
+const App =()=>{
+    return (<div>how are you</div>)
+}
+export default App;
---
+++ learnUI/src/index.js
@@ -0,0 +1,7 @@
+import reactDOM from 'react-dom';
+import React from 'react';
+import App from './app';
+
+window.renderLearnUI=()=>{
+    reactDOM.render(<App/>,document.getElementById("root"));
+};l̥
